57df98b555c15df9acd19aec8bf9e269ca1e3317
Gabriel Kobayashi <gabrielkobayashi11@gmail.com>
2026-03-12 09:38:20 -0300

    feat: adiciona cor no resultado e classificação do IMC

--- calc_imc_react/src/App.jsx
+++ calc_imc_react/src/App.jsx
@@ -18,16 +18,29 @@ function App() {
     const imcResult = (weightFloat / (heightFloat * heightFloat)).toFixed(1);
 
     setImc(imcResult);
+
+    data.forEach((item) => {
+      if (imcResult >= item.min && imcResult <= item.max) {
+        setInfo(item.info);
+        setInfoClass(item.infoClass);
+      }
+    });
+
+    if (!info) return;
   };
 
   const [imc, setImc] = useState("");
-  const [info, setinfo] = useState("");
+  const [info, setInfo] = useState("");
   const [infoClass, setInfoClass] = useState("");
 
   return (
     <>
       <div className="container">
-        {!imc ? <ImcCalc calcImc={calcImc} /> : <ImcTable data={data} />}
+        {!imc ? (
+          <ImcCalc calcImc={calcImc} />
+        ) : (
+          <ImcTable data={data} imc={imc} info={info} infoClass={infoClass} />
+        )}
       </div>
     </>
   );
